57761e13ab50ced7bef20ba98a3b0b26f0aa31a0
Anton Lomovatskyi <RoffyMonsta@users.noreply.github.com>
2023-11-02 22:36:18 +0200

    build and deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const { merge } = require("webpack-merge");
+const path = require('path');
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -14,6 +15,10 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
+    },
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
